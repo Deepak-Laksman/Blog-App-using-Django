8f99e6370bad9de141607b035cb110ecddd08151
Deepak-Laksman <deepaklaksmanj@gmail.com>
2022-02-13 20:09:20 +0530

    User Registration Ongoing

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        "author" : "CoreyMS",
-        "title" : "Blog Post 1",
-        "content" : "First Post Content",
-        "date_posted" : "Aug 27th 2018"
-    },
-    {
-        "author" : "JaneDoe",
-        "title" : "Blog Post 2",
-        "content" : "Second Post Content",
-        "date_posted" : "Aug 28th 2018"
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
