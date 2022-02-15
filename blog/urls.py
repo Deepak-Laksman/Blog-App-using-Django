fc4851f1d533bc080a1592311fc64c5d7c16df99
Deepak-Laksman <deepaklaksmanj@gmail.com>
2022-02-16 00:04:53 +0530

    email server setup pending

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,19 @@ from django.urls import path
 from .views import (
     PostListView, 
     PostDetailView, 
-    PostCreateView)
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+    )
 from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name = "blog-home"),
     path('post/<int:pk>/', PostDetailView.as_view(), name = "post-detail"),
-    path('post/new/', PostCreateView.as_view(), name = "post-create"),
+    path('post/new/', PostCreateView.as_view(), name = "post-create"),   # post_form.html template
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = "post-update"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name = "post-delete"),      # post_confirm_delete.html template
+    path('user/<str:username>/', UserPostListView.as_view(), name = "user-posts"),
     path('about/', views.about, name = "blog-about"),
 ]
